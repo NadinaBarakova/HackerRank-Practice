a0cc88dd41524dd8ddae6652cf47324f2d1fa3f1
NadinaBarakova <68910045+NadinaBarakova@users.noreply.github.com>
2021-10-14 16:07:42 +0300

    Add files via upload

---
+++ One-week-prep-kit/Mini-Max-Sum/MiniMaxSumProgram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace MiniMaxSum
+{
+    class MiniMaxSumProgram
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+            long min = long.MinValue;
+            long max = long.MaxValue;
+            long sumMin = 0;
+            long sumMax = 0;
+            List<int> copyArr = new List<int>(arr);
+
+            List<int> dist = new List<int>(arr.Distinct());
+            if (dist.Count == 1)
+            {
+                sumMax = sumMin = dist[0] * 4;
+            }
+            else
+            {
+                while (arr.Count > 1 || copyArr.Count > 1)
+                {
+                    if (arr.Min() > min)
+                    {
+                        min = arr.Min();
+                        sumMin += min;
+                        arr.Remove(arr.Min());
+                    }
+                    if (copyArr.Max() < max)
+                    {
+                        max = copyArr.Max();
+                        sumMax += max;
+                        copyArr.Remove(copyArr.Max());
+                    }
+                }
+            }
+
+            List<long> result = new List<long>();
+            result.Add(sumMin);
+            result.Add(sumMax);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ One-week-prep-kit/Plus-Minus/PlusMinusProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OneWeekPreparationKit
+{
+    class PlusMinusProgram
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            int positiveCount = 0;
+            int negativeCount = 0;
+            int zeroCount = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positiveCount++;
+                }
+                if (arr[i] < 0)
+                {
+                    negativeCount++;
+                }
+                if (arr[i] == 0)
+                {
+                    zeroCount++;
+                }
+            }
+            decimal positive = (decimal)positiveCount / arr.Count*1.000000M;
+            decimal negative = (decimal)negativeCount / arr.Count * 1.000000M;
+            decimal zero = (decimal)zeroCount / arr.Count * 1.000000M;
+
+            positive = Math.Round(positive, 6);
+            negative = Math.Round(negative, 6);
+            zero = Math.Round(zero, 6);
+
+            Console.WriteLine(positive);
+            Console.WriteLine(negative);
+            Console.WriteLine(zero);
+
+        }
+    }
+}
