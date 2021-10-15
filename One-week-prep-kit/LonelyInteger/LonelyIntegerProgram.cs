6ef2e968cf2fa146797f226e879739130788d1ec
NadinaBarakova <68910045+NadinaBarakova@users.noreply.github.com>
2021-10-15 07:09:33 +0300

    Add files via upload

---
+++ One-week-prep-kit/LonelyInteger/LonelyIntegerProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LonelyInteger
+{
+    class LonelyIntegerProgram
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> a = Console.ReadLine().Split().Select(int.Parse).ToList();
+            a.Add(-1);
+            a.Sort();
+            a.Reverse();
+            int result = 0;
+
+            for (int i = 0; i < a.Count; i += 2)
+            {
+                if (a[i] != a[i + 1] || (a[i] == a[a.Count - 2] && a[i] != a[i - 1]))
+                {
+                    result =a [i];
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
