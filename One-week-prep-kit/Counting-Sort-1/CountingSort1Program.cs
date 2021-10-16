fb9519515c51e13a611d4681491ada49e3b89960
NadinaBarakova <68910045+NadinaBarakova@users.noreply.github.com>
2021-10-16 13:53:36 +0300

    Add files via upload

---
+++ One-week-prep-kit/Counting-Sort-1/CountingSort1Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingSort1
+{
+    class CountingSort1Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int[] result = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                result[i] = 0;
+            }
+            int item = 1;
+
+            int x = arr.Count / 100;
+            while (x > 0)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (result[arr[i]] == 0)
+                    {
+                        result[arr[i]] = item;
+                    }
+                    else if (result[arr[i]] != 0)
+                    {
+                        result[arr[i]] += item;
+                        item = 1;
+                    }
+                }
+                arr.RemoveRange(0, 100);
+                x--;
+            }
+            Console.WriteLine(string.Join(" ", result.ToList()));
+
+        }
+    }
+}
